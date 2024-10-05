c7984c868de57333d099a4769993e3eaec32b81e
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-05 10:14:40 -0500

    erase gridtemplatecolumns extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,6 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
-            gridTemplateColumns: {
-                16: "repeat(16,minmax(0,1fr))",
-            },
             fontFamily: {
                 sans: ["Karla", ...defaultTheme.fontFamily.sans],
                 mono: ["Martian Mono", ...defaultTheme.fontFamily.mono],
