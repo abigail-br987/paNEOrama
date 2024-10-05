666654c05c942f27522a69379354104f7a9de49f
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-05 09:25:05 -0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className='bg-red-500'>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1 className='text-lg'>Vite + React</h1>
