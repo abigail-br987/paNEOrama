739ba09e0c3bfb78a885fb6d7b7a25c7a5766751
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-05 09:14:07 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className='text-lg'>Vite + React</h1>
+      <p className="read-the-docs bg-black p-6">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
