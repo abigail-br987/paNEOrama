8de7e3efecc0c3beff9576dbecf4e76707d7d415
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-05 11:03:00 -0500

    checkboxoption component

---
+++ src/components/panel/SmallComponents/CheckboxOption.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Tooltip from './Tooltip';
+
+interface CheckboxOptionProps {
+  name: string;
+  checked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  tooltipText?: string;
+}
+
+const CheckboxOption: React.FC<CheckboxOptionProps> = ({
+  name,
+  checked,
+  onChange,
+  label,
+  tooltipText,
+}) => (
+  <label className="flex items-center space-x-2">
+    <input
+      type="checkbox"
+      name={name}
+      className="form-checkbox h-4 w-4"
+      checked={checked}
+      onChange={onChange}
+    />
+    {tooltipText ? (
+      <Tooltip tooltipText={tooltipText}>
+        <span>{label}</span>
+      </Tooltip>
+    ) : (
+      <span className="text-sm">{label}</span>
+    )}
+  </label>
+);
+
+export default CheckboxOption;
