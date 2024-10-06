5d094370d09e3909b085257c1abbcb1ab123935d
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-06 09:06:41 -0500

    idk how to fix this

--- src/components/panel/InfoPanel/AddToTransmission.tsx
+++ src/components/panel/InfoPanel/AddToTransmission.tsx
@@ -4,43 +4,40 @@ import { IoMdAddCircle } from "react-icons/io";
 import GlobalStore from "@/lib/context/GlobalStore";
 import { useContext } from "react";
 import { useState } from "react";
+import React from "react";
 
-function AddToTransmission() {
+const AddToTransmission = React.memo(() => {
   const [{ selectedObjectData, favoriteData }, updateStore] =
     useContext(GlobalStore);
   const [list, setList] = useState<[]>([]);
 
-  console.log(list, "list rightnoowww")
-  console.log(favoriteData, "dataa");
-
   function addThings() {
-    console.log("addThings called"); // Debug log
+    console.log("addThings called");
     console.log(selectedObjectData, "before addThings");
     console.log(list, "current list"); 
     const updatedList = [...list, selectedObjectData];
     console.log(selectedObjectData, "selected");
-    console.log(updatedList, "updatedList")
-      //@ts-ignore
+    console.log(updatedList, "updatedList");
     setList(updatedList);
     updateStore({ favoriteData: updatedList });
-}
-  //@ts-ignore
+  }
+
   const isInList = list.some((item) => item.spkid === selectedObjectData.spkid);
 
-
   return (
-    <>
-      <PanelThing className="cursor-pointer px-3 self-end">
-        <TitleOptions
+    <PanelThing className="cursor-pointer px-3 self-end">
+      <TitleOptions>
+        <button
+          className={`${isInList ? "opacity-50 cursor-not-allowed" : ""}`}
+          onClick={addThings}
+          disabled={isInList}
         >
-          <button className={`${isInList ? "opacity-50 cursor-not-allowed" : ""}`} onClick={addThings} disabled={isInList}>
           <IoMdAddCircle className="text-md inline-block" /> {" "}
           Add To Transmission
-          </button>
-        </TitleOptions>
-      </PanelThing>
-    </>
+        </button>
+      </TitleOptions>
+    </PanelThing>
   );
-}
+});
 
 export default AddToTransmission;
