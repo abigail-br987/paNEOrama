113b31a03afd99a24c7f7441a5128b72f78331bb
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-06 07:33:48 -0500

    list of favorite asteroids

---
+++ src/components/panel/InfoPanel/AddToTransmission.tsx
@@ -0,0 +1,46 @@
+import PanelThing from "../SmallComponents/PanelThing";
+import TitleOptions from "../SmallComponents/TitleOption";
+import { IoMdAddCircle } from "react-icons/io";
+import GlobalStore from "@/lib/context/GlobalStore";
+import { useContext } from "react";
+import { useState } from "react";
+
+function AddToTransmission() {
+  const [{ selectedObjectData, favoriteData }, updateStore] =
+    useContext(GlobalStore);
+  const [list, setList] = useState<[]>([]);
+
+  console.log(list, "list rightnoowww")
+  console.log(favoriteData, "dataa");
+
+  function addThings() {
+    console.log("addThings called"); // Debug log
+    console.log(selectedObjectData, "before addThings");
+    console.log(list, "current list"); 
+    const updatedList = [...list, selectedObjectData];
+    console.log(selectedObjectData, "selected");
+    console.log(updatedList, "updatedList")
+
+    setList(updatedList);
+    updateStore({ favoriteData: updatedList });
+}
+  //@ts-ignore
+  const isInList = list.some((item) => item.spkid === selectedObjectData.spkid);
+
+
+  return (
+    <>
+      <PanelThing className="cursor-pointer px-3 self-end">
+        <TitleOptions
+        >
+          <button className={`${isInList ? "opacity-50 cursor-not-allowed" : ""}`} onClick={addThings} disabled={isInList}>
+          <IoMdAddCircle className="text-md inline-block" /> {" "}
+          Add To Transmission
+          </button>
+        </TitleOptions>
+      </PanelThing>
+    </>
+  );
+}
+
+export default AddToTransmission;
