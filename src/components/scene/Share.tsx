f5a02c52bbe7ed1945b4ae5b187e58901451721a
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-05 22:14:21 -0500

    mini changes

--- src/components/scene/Share.tsx
+++ src/components/scene/Share.tsx
@@ -7,68 +7,70 @@ import CloseButton from "../panel/SmallComponents/CloseButton";
 
 const Share = () => {
     const link = "https:";
-    const [showPanel, setShowPanel] = useState(false);
+  const [showPanel, setShowPanel] = useState(false);
     const [message, setMessage] = useState("");
 
-    const CopyLink = () => {
-        navigator.clipboard.writeText(link);
+  const CopyLink = () => {
+    navigator.clipboard.writeText(link);
         setMessage("Link copied to clipboard!");
         setTimeout(() => setMessage(""), 3000);
-    };
+  };
 
-    const Embed = () => {
-        const embedCode = `<iframe src="${link}" frameborder="0" width="600" height="400"></iframe>`;
-        navigator.clipboard.writeText(embedCode);
+  const Embed = () => {
+    const embedCode = `<iframe src="${link}" frameborder="0" width="600" height="400"></iframe>`;
+    navigator.clipboard.writeText(embedCode);
         setMessage("Embed code copied to clipboard!");
         setTimeout(() => setMessage(""), 3000);
-    };
+  };
 
-    return (
-        <>
-            <div className="absolute bottom-0  text-white break-words px-6 py-4 rounded-md">
-                <PanelThing
-                    className="cursor-pointer px-3"
-                    onClick={() => setShowPanel(true)}
+  return (
+    <>
+      <>
+        <PanelThing
+          className="cursor-pointer px-3 self-end"
+          onClick={() => setShowPanel(true)}
+        >
+          <TitleOptions>Share!</TitleOptions>
+        </PanelThing>
+      </>
+      {showPanel && (
+          <div className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center">
+
+        <LargePanel medium={true}>
+          <div
+            className="flex flex-col relative h-full w-full bg-black
+           rounded-lg text-white"
+          >
+            <div className="z-10 p-8">
+              <TitleOptions>Share our Orrery!</TitleOptions>
+              <div className="flex items-center space-x-4">
+                <div
+                  className="border p-2 px-4 hover:bg-orange-700 cursor-pointer flex items-center rounded-md"
+                  onClick={CopyLink}
                 >
-                    <TitleOptions>Share!</TitleOptions>
-                </PanelThing>
-            </div>
-            {showPanel && (
-                <LargePanel medium={true}>
-                    <div
-                        className="flex flex-col relative h-full w-full bg-black
-           rounded-lg text-white "
-                    >
-                        <div className="z-10 p-8">
-                            <TitleOptions>Share our Orrery!</TitleOptions>
-                            <div className="flex items-center space-x-4">
-                                <div
-                                    className="border p-2 px-4 hover:bg-orange-700 cursor-pointer flex items-center rounded-md"
-                                    onClick={CopyLink}
-                                >
-                                    <span>Link</span>
-                                </div>
-                                <div
-                                    className="border p-2 px-4 hover:bg-orange-700 cursor-pointer flex items-center rounded-md"
-                                    onClick={Embed}
-                                >
-                                    <span>Embed</span>
-                                </div>
+                  <span>Link</span>
+                </div>
+                <div
+                  className="border p-2 px-4 hover:bg-orange-700 cursor-pointer flex items-center rounded-md"
+                  onClick={Embed}
+                >
+                  <span>Embed</span>
+                </div>
 
-                                <span className="opacity-75">{message || `Link: ${link}`}</span>
-                            </div>
-                        </div>
-                        <CloseButton onClick={() => setShowPanel(false)} />
-                        <img
-                            src={ss}
-                            className="absolute rounded-lg object-cover w-full h-full"
-                            alt="Orrery"
-                        />
-                    </div>
-                </LargePanel>
-            )}
-        </>
-    );
+                <span className="opacity-75">{message || `Link: ${link}`}</span>
+              </div>
+            </div>
+            <CloseButton onClick={() => setShowPanel(false)} />
+            <img
+              src={ss}
+              className="absolute rounded-lg object-cover w-full h-full"
+              alt="Orrery"
+            />
+          </div>
+        </LargePanel></div>
+      )}
+    </>
+  );
 };
 
 export default Share;
