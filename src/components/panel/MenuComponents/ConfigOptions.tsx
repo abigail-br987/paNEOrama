705841b06785701c432474c220ea88697484e57a
Grazen0 <josedanielgrayson@proton.me>
2024-10-06 15:23:13 -0500

    better ui 2

--- src/components/panel/MenuComponents/ConfigOptions.tsx
+++ src/components/panel/MenuComponents/ConfigOptions.tsx
@@ -1,59 +1,52 @@
 import GlobalStore, { Store } from "@/lib/context/GlobalStore";
 import { useContext } from "react";
-import TitleOptions from "../SmallComponents/TitleOption";
-import { useMemo } from "react";
-import { useCallback } from "react";
 import Tooltip from "../SmallComponents/Tooltip";
 import PanelThing from "../SmallComponents/PanelThing";
+import classNames from "classnames";
+
+const OPTIONS: Store["view"][] = ["Planets", "NEAs", "NECs", "PHAs"];
 
 function ConfigOptions() {
-  const [{ view }, updateStore] = useContext(GlobalStore);
-  const options: Store["view"][] = useMemo(
-    () => ["Planets", "NEAs", "NECs", "PHAs"],
-    []
-  );
+    const [{ view }, updateStore] = useContext(GlobalStore);
 
-  const forTooltip = [
-    "Planets",
-    "Near Earth Asteroids",
-    "Near Earth Comets",
-    "Potentially Hazardous Asteroids",
-  ];
+    const forTooltip = [
+        "Planets",
+        "Near Earth Asteroids",
+        "Near Earth Comets",
+        "Potentially Hazardous Asteroids",
+    ];
 
-  const handleButtonClick = 
-    (option: Store["view"]) => {
-      updateStore({
-        view: option,
-        neoSelected: null,
-        selectedPlanet: null,
-        showLabels: true,
-        showOrbits: true,
-        radialLines: true,
-      });
+    const handleButtonClick = (option: Store["view"]) => {
+        updateStore({
+            view: option,
+            neoSelected: null,
+            selectedPlanet: null,
+            showLabels: true,
+            showOrbits: true,
+            radialLines: true,
+        });
     };
 
-  return (
-    <div className="grid grid-cols-2 gap-2">
-      {options.map((option, index) => {
-        const className =
-          view === option ? "bg-orange-600 cursor-pointer" : "cursor-pointer";
-        const tooltipText = forTooltip[index];
-        return (
-          <PanelThing
-            key={option}
-            onClick={() => handleButtonClick(option)}
-            className={className}
-          >
-            <TitleOptions>
-              <Tooltip tooltipText={tooltipText}>
-                <button>{option}</button>
-              </Tooltip>
-            </TitleOptions>
-          </PanelThing>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="grid grid-cols-2 gap-2">
+            {OPTIONS.map((option, index) => (
+                <PanelThing
+                    key={option}
+                    onClick={() => handleButtonClick(option)}
+                    className={classNames(
+                        "cursor-pointer text-center py-1 px-4 font-extrabold",
+                        view === option
+                            ? "bg-orange-600 bg-opacity-100"
+                            : "hover:bg-orange-900",
+                    )}
+                >
+                    <Tooltip tooltipText={forTooltip[index]}>
+                        <button className="w-full text-center">{option}</button>
+                    </Tooltip>
+                </PanelThing>
+            ))}
+        </div>
+    );
 }
 
 export default ConfigOptions;
