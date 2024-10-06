5981753d896196c47279cb5b62233c8cb8e7063c
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-06 07:48:06 -0500

    panel in the same component

--- src/components/panel/ObservableNeos.tsx
+++ src/components/panel/ObservableNeos.tsx
@@ -1,15 +1,18 @@
-import { useContext, useEffect, useMemo } from 'react';
-import CloseButton from './SmallComponents/CloseButton';
-import GlobalStore from '@/lib/context/GlobalStore';
-import 'leaflet/dist/leaflet.css';
-import { useState } from 'react';
-import ObservableNEASMap from './MapComponents/ObservableNEASMap';
-import { useRef } from 'react';
-import axios from 'axios';
-import { headers } from './MapComponents/FormatTable';
-import ObservableNEASTable from './MapComponents/ObservableNEASTable';
-import { formatDate } from '@/lib/utils/date';
-import LargePanel from './SmallComponents/LargePanel';
+import { useContext, useEffect, useMemo } from "react";
+import CloseButton from "./SmallComponents/CloseButton";
+import GlobalStore from "@/lib/context/GlobalStore";
+import "leaflet/dist/leaflet.css";
+import { useState } from "react";
+import ObservableNEASMap from "./MapComponents/ObservableNEASMap";
+import { useRef } from "react";
+import axios from "axios";
+import { headers } from "./MapComponents/FormatTable";
+import ObservableNEASTable from "./MapComponents/ObservableNEASTable";
+import { formatDate } from "@/lib/utils/date";
+import PanelThing from "./SmallComponents/PanelThing";
+import TitleOptions from "./SmallComponents/TitleOption";
+import LargePanel from "./SmallComponents/LargePanel";
+import { BsFillGeoAltFill } from "react-icons/bs";
 interface Position {
   lat: number;
   lng: number;
@@ -17,7 +20,7 @@ interface Position {
 
 function ObservableNeos() {
   const [{}, updateStore] = useContext(GlobalStore);
-
+  const [showPanel, setShowPanel] = useState(false);
   const handleClose = () => {
     updateStore({ showToObserve: false });
   };
@@ -31,19 +34,19 @@ function ObservableNeos() {
     if (position) {
       const fetchData = async () => {
         try {
-          const proxyUrl = 'https://corsproxy.io/?';
+          const proxyUrl = "https://corsproxy.io/?";
           const apiUrl = encodeURIComponent(
             `https://ssd-api.jpl.nasa.gov/sbwobs.api?lat=${position.lat}&lon=${position.lng}&obs-time=${datetoday}&sb-group=neo&sb-kind=a&maxoutput=10&output-sort=vmag`
           );
           const response = await axios.get(`${proxyUrl}${apiUrl}`);
           setData(response.data);
         } catch (error) {
-          console.error('Error:', error);
+          console.error("Error:", error);
         }
       };
       fetchData();
     } else {
-      console.error('Error: Position is undefined');
+      console.error("Error: Position is undefined");
     }
   }, [position]);
 
@@ -66,62 +69,75 @@ function ObservableNeos() {
   );
 
   return (
-    <LargePanel medium> 
-      <div className="absolute backdrop-blur-3xl z-10 text-xs rounded-md text-black m-10 p-6 border max-w-full overflow-auto">
-        <h1 className="text-lg font-extrabold mb-2 uppercase">
-          ObservaNEAS{' '}
-          <a
-            href="https://ssd-api.jpl.nasa.gov/doc/sbwobs.html"
-            className="text-xs opacity-50 hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-            NASA Small-Body Observability API
-          </a>
-        </h1>
-        {position && position.lat != null && position.lng != null ? (
-          <>
-            {dragged ? (
-              <p>
-                Let's explore the top 10 observable NEAs from{' '}
-                <span className="font-bold">
-                  {' '}
-                  lat: {position.lat}, lon: {position.lng}{' '}
-                </span>
-                based on their visual magnitude
-              </p>
+    <>
+      <PanelThing
+        className="cursor-pointer px-3 self-end"
+        onClick={() => setShowPanel(true)}
+      >
+
+        <TitleOptions><BsFillGeoAltFill className="inline-block" /> ObservableNEAS</TitleOptions>
+
+      </PanelThing>
+
+      {showPanel && (
+        <LargePanel medium>
+          <div className="absolute backdrop-blur-3xl z-10 text-xs rounded-md text-black m-10 p-6 border max-w-full overflow-auto">
+            <h1 className="text-lg font-extrabold mb-2 uppercase">
+              ObservaNEAS{" "}
+              <a
+                href="https://ssd-api.jpl.nasa.gov/doc/sbwobs.html"
+                className="text-xs opacity-50 hover:underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                NASA Small-Body Observability API
+              </a>
+            </h1>
+            {position && position.lat != null && position.lng != null ? (
+              <>
+                {dragged ? (
+                  <p>
+                    Let's explore the top 10 observable NEAs from{" "}
+                    <span className="font-bold">
+                      {" "}
+                      lat: {position.lat}, lon: {position.lng}{" "}
+                    </span>
+                    based on their visual magnitude
+                  </p>
+                ) : (
+                  <p>
+                    The top 10 observable NEAs from your location based on their
+                    visual magnitude{" "}
+                    <span className="font-bold">
+                      {" "}
+                      (lat: {position.lat}, lon: {position.lng})
+                    </span>
+                  </p>
+                )}{" "}
+              </>
             ) : (
               <p>
-                The top 10 observable NEAs from your location based on their
-                visual magnitude{' '}
-                <span className="font-bold">
-                  {' '}
-                  (lat: {position.lat}, lon: {position.lng})
-                </span>
+                Let's see the top 10 observable NEAs from your location! ({" "}
+                {datetoday} )
               </p>
-            )}{' '}
-          </>
-        ) : (
-          <p>
-            Let's see the top 10 observable NEAs from your location! ({' '}
-            {datetoday} )
-          </p>
-        )}
-        <ObservableNEASTable
-          data={data}
-          headers={headers}
-        ></ObservableNEASTable>
-      </div>
-      <CloseButton onClick={handleClose} />
-      <ObservableNEASMap
-        setPosition={setPosition}
-        position={position}
-        draggable={draggable}
-        eventHandlers={eventHandlers}
-        markerRef={markerRef}
-      />
-    </LargePanel>
+            )}
+            <ObservableNEASTable
+              data={data}
+              headers={headers}
+            ></ObservableNEASTable>
+          </div>
+          <CloseButton onClick={() => setShowPanel(false)} />
+          <ObservableNEASMap
+            setPosition={setPosition}
+            position={position}
+            draggable={draggable}
+            eventHandlers={eventHandlers}
+            markerRef={markerRef}
+          />
+        </LargePanel>
+      )}
+    </>
   );
 }
 
--- src/components/panel/OrbitSimulator.tsx
+++ src/components/panel/OrbitSimulator.tsx
@@ -1,83 +1,84 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { Fragment, useState } from 'react';
-import { Line } from '@react-three/drei';
-import Stars from '../scene/object/Stars';
-import GlobalStore from '@/lib/context/GlobalStore';
-import { useContext } from 'react';
-import CloseButton from './SmallComponents/CloseButton';
-import { Vector3 } from 'three';
-import LargePanel from './SmallComponents/LargePanel';
-import { useMemo } from 'react';
-import { PlaneGeometry } from 'three';
-import Dropdown from './SmallComponents/Dropdown';
-import TitleOptions from './SmallComponents/TitleOption';
-import { computeOrbitPoints } from '@/lib/utils/compute-orbit-points';
-import { Orbit } from '../scene/object/Orbit';
-
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Fragment, useState } from "react";
+import { Line } from "@react-three/drei";
+import Stars from "../scene/object/Stars";
+import GlobalStore from "@/lib/context/GlobalStore";
+import { useContext } from "react";
+import CloseButton from "./SmallComponents/CloseButton";
+import { Vector3 } from "three";
+import LargePanel from "./SmallComponents/LargePanel";
+import { useMemo } from "react";
+import { PlaneGeometry } from "three";
+import Dropdown from "./SmallComponents/Dropdown";
+import TitleOptions from "./SmallComponents/TitleOption";
+import { computeOrbitPoints } from "@/lib/utils/compute-orbit-points";
+import { Orbit } from "../scene/object/Orbit";
+import PanelThing from "./SmallComponents/PanelThing";
+import { BsPencilSquare } from "react-icons/bs";
 const orbitalParameters = [
   {
-    label: 'Semi-Major Axis (a)',
+    label: "Semi-Major Axis (a)",
     value: 1,
     min: 0.5,
     max: 4,
     step: 0.1,
     description:
-      'Longest radius of an elliptical orbit around a celestial body.',
+      "Longest radius of an elliptical orbit around a celestial body.",
   },
   {
-    label: 'Eccentricity (e)',
+    label: "Eccentricity (e)",
     value: 0.1,
     min: 0,
     max: 0.9,
     step: 0.01,
-    description: 'Measure of the deviation of the orbit from a perfect circle.',
+    description: "Measure of the deviation of the orbit from a perfect circle.",
   },
   {
-    label: 'Inclination (i)',
+    label: "Inclination (i)",
     value: 45,
     min: 0,
     max: 90,
     step: 1,
-    description: 'Angle between the orbit plane and the reference plane.',
+    description: "Angle between the orbit plane and the reference plane.",
   },
   {
-    label: 'Argument of Periapsis (om)',
+    label: "Argument of Periapsis (om)",
     value: 0,
     min: 0,
     max: 360,
     step: 1,
     description:
-      'Angle between the direction of periapsis and the ascending node.',
+      "Angle between the direction of periapsis and the ascending node.",
   },
   {
-    label: 'Longitude of Ascending Node (w)',
+    label: "Longitude of Ascending Node (w)",
     value: 0,
     min: 0,
     max: 360,
     step: 1,
     description:
-      'Angle between the reference direction and the ascending node.',
+      "Angle between the reference direction and the ascending node.",
   },
 ];
 
 function CustomAxes({ length = 15 }) {
   const axisData = [
     {
-      axis: 'x',
-      color: 'blue',
+      axis: "x",
+      color: "blue",
       rotation: [0, Math.PI / 2, 0],
       linePoints: [new Vector3(-length, 0, 0), new Vector3(length, 0, 0)],
     },
     {
-      axis: 'y',
-      color: 'purple',
+      axis: "y",
+      color: "purple",
       rotation: [Math.PI / 2, 0, 0],
       linePoints: [new Vector3(0, -length, 0), new Vector3(0, length, 0)],
     },
     {
-      axis: 'z',
-      color: 'red',
+      axis: "z",
+      color: "red",
       rotation: [0, 0, 0],
       linePoints: [new Vector3(0, 0, -length), new Vector3(0, 0, length)],
     },
@@ -110,8 +111,9 @@ function CustomAxes({ length = 15 }) {
 
 function OrbitSimulator({ scale = 0.001 }) {
   const [{}, updateStore] = useContext(GlobalStore);
+  const [showPanel, setShowPanel] = useState(false);
   const [parameters, setParameters] = useState(
-    orbitalParameters.map(param => param.value)
+    orbitalParameters.map((param) => param.value)
   );
   const handleClose = () => {
     updateStore({ showOrbitSimulator: false });
@@ -133,10 +135,19 @@ function OrbitSimulator({ scale = 0.001 }) {
   };
 
   return (
+    <>
+      <PanelThing
+        className="cursor-pointer px-3"
+        onClick={() => setShowPanel(true)}
+      >
+        <TitleOptions><BsPencilSquare className="inline-block" /> Orbit Creator</TitleOptions>
+      </PanelThing>
+
+      {showPanel && (
 
       <LargePanel medium>
-        <CloseButton onClick={handleClose} />
-        <div className="absolute backdrop-blur-sm z-10 rounded-md m-10  p-6 border max-w-lg">
+            <CloseButton onClick={() => setShowPanel(false)} />
+            <div className="absolute backdrop-blur-sm z-10 rounded-md m-10  p-6 border max-w-lg">
           <TitleOptions>Create your own orbit!</TitleOptions>
           <p>Orbital Parameters are fun!</p>
           {orbitalParameters.map((param, index) => (
@@ -157,7 +168,7 @@ function OrbitSimulator({ scale = 0.001 }) {
                 max={param.max}
                 step={param.step}
                 value={parameters[index]}
-                onChange={e => handleChange(index, e.target.value)}
+                onChange={(e) => handleChange(index, e.target.value)}
                 className="w-full"
               />
             </div>
@@ -194,8 +205,8 @@ function OrbitSimulator({ scale = 0.001 }) {
             </mesh>
           </Canvas>
         </div>
-      </LargePanel>
-
+      </LargePanel>)}
+    </>
   );
 }
 
