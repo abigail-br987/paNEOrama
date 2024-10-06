10943808897708f807918912c41ac83cf779497c
abigail_br987 <abigailbrionesaranda@gmail.com>
2024-10-05 22:23:07 -0500

    update

--- src/components/scene/SolarSystem.tsx
+++ src/components/scene/SolarSystem.tsx
@@ -1,38 +1,48 @@
-import { useContext } from "react";
-import { Canvas } from "@react-three/fiber";
-import GlobalStore from "@/lib/context/GlobalStore";
-import NaveCanvas from "./NaveCanvas";
-import Sun from "./object/Sun";
-import Light from "./object/Light";
-import Stars from "./object/Stars";
-import Planets from "./object/Planets";
+import { useContext } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Sun from './object/Sun';
+import GlobalStore from '@/lib/context/GlobalStore';
+import Stars from './object/Stars';
+import Light from './object/Light';
+import Planets from './object/Planets';
+import Grid from './object/Grid';
+import AllObjects from './object/AllObjects';
+import SmallCelestialObject from './object/SmallCelestialObject';
+import NaveCanvas from './NaveCanvas';
+
 const SolarSystem = () => {
-    const [{ view, start }] = useContext(GlobalStore);
+  const [{ view, seeAllObjects, start }] = useContext(GlobalStore);
 
-    return (
-        <div className="absolute w-screen h-screen">
+  return (
+    <div className="absolute w-screen h-screen">
             <div className="w-full h-full absolute">
-                <Canvas
-                    gl={{ antialias: false }}
-                    frameloop="demand"
-                    camera={{
-                        position: [-1000, 500, 1000],
-                        near: 100,
-                        far: 100_000,
-                        fov: 45,
-                    }}
-                >
-                    {!start && <NaveCanvas />}
-
-                    <Sun />
-
-                    <Light />
-                    <Stars number={80000} size={3} />
-                    <Planets />
-                </Canvas>
-            </div>
-        </div>
-    );
+        <Canvas
+          gl={{ antialias: false }}
+          frameloop="demand"
+          camera={{
+            position: [-1000, 500, 1000],
+            near: 100,
+            far: 100_000,
+            fov: 45,
+          }}
+        >
+          {start ? (
+            <>
+              <Grid />
+              {view !== 'Planets' && <SmallCelestialObject />}
+              {view !== 'Planets' && seeAllObjects && <AllObjects />}
+            </>
+          ) : (
+            <NaveCanvas />
+          )}
+          <Sun />
+          <Planets />
+          <Stars number={80000} size={3} />
+          <Light />
+        </Canvas>
+      </div>
+    </div>
+  );
 };
 
 export default SolarSystem;
